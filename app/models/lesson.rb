3cdae4f50a824fa80259d9d49aa496aa7dbc291e
issarakoat <weeravut.issarakoat@gmail.com>
2020-12-18 18:38:27 +0000

    86 pubdit for lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,7 +1,16 @@
 class Lesson < ApplicationRecord
   belongs_to :course
   validates :title, :instruction, :course, presence: true
+  
+  has_rich_text :instruction
 
   extend FriendlyId
   friendly_id :title, use: :slugged
+  
+  include PublicActivity::Model
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
+
+  def to_s
+    title
+  end
 end
