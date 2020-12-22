92923d2d4307a8468c69ad325d614684cc51e786
issarakoat <weeravut.issarakoat@gmail.com>
2020-12-22 05:50:01 +0000

    140. UserCourse table - track if a user has seen a lesson. Migration and setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   
   has_many :courses
   has_many :enrollments
+  has_many :user_lessons
   
   extend FriendlyId
   friendly_id :email, use: :slugged
@@ -38,6 +39,12 @@ class User < ApplicationRecord
   
   def buy_course(course)
     self.enrollments.create(course: course, price: course.price)
+  end
+  
+  def view_lesson(lesson)
+    unless self.user_lessons.where(lesson: lesson).any?
+      self.user_lessons.create(lesson: lesson)
+    end
   end
   
   private
