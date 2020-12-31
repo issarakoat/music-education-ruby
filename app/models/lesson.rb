874172fe3de3c98a10097cd2a314476cd0707e66
issarakoat <weeravut.issarakoat@gmail.com>
2020-12-31 15:19:06 +0000

    205 styling lessons, video validation

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -16,6 +16,11 @@ class Lesson < ApplicationRecord
   validates :video_thumbnail,
     content_type: ['image/png', 'image/jpg', 'image/jpeg'], 
     size: { less_than: 10000.kilobytes , message: 'size should be under 10000 kilobytes' }
+    
+  #validates :video_thumbnail, presence: true, if: :video_present?
+  #def video_present?
+  #  self.video.present?
+  #end
 
   extend FriendlyId
   friendly_id :title, use: :slugged
